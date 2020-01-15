a721240ff27a6e8d4d129584e567c9c4c4cff02c
jinenab <jinenab123@gmail.com>
2020-01-15 15:14:22 +0100

    my nav is responsive now

---
+++ src/components/Nav/nav.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <h1>Vegano</h1>
+      <div className="links">
+<a href="#">About</a>
+<a href="#">Contact</a>
+<a href="#">Meals</a>
+<a href="#">Restaurant</a>
+       </div>
+    </div>
+  );
+}
+
+export default Nav
+
